60af82109a2933d157b0f0d234fa9df415d18c91
Will Acton <will@willacton.com>
2020-05-03 21:12:16 -0700

    organize tests

--- src/test.ts
+++ src/test.ts
@@ -2,12 +2,10 @@ import { ref, deref, set, alter, transaction } from "./stm2";
 
 test("create two refs, change them, commit them", () => {
   let foo = ref(0);
-
   let bar = ref(2);
+  let tx = transaction();
 
-  let tx0 = transaction();
-
-  tx0.add(() => {
+  tx.add(() => {
     set(foo, 2);
     alter(bar, (bar) => deref(foo) * bar);
   });
@@ -15,75 +13,92 @@ test("create two refs, change them, commit them", () => {
   expect(deref(foo)).toBe(0);
   expect(deref(bar)).toBe(2);
 
-  tx0.commit();
+  tx.commit();
 
   expect(deref(foo)).toBe(2);
   expect(deref(bar)).toBe(4);
 });
 
-test("abort and retry", () => {
+describe("abort and retry", () => {
   let foo = ref(0);
-  let tx1 = transaction();
+  let tx = transaction();
 
-  tx1.add(() => {
-    if (deref(foo) === 0) {
-      throw new Error("Invalid foo!");
-    }
-    set(foo, -1);
-  });
-
-  expect(deref(foo)).toBe(0);
-
-  let error;
-  try {
-    tx1.commit();
-  } catch (e) {
-    error = e;
-  }
-  expect(error.message).toBe("Invalid foo!");
-
-  expect(deref(foo)).toBe(0);
-
-  try {
-    tx1.commit();
-  } catch (e) {
-    error = e;
-  }
-  expect(error.message).toBe(
-    "Cannot commit transaction which has been aborted. Retry it first"
-  );
-
-  transaction()
-    .add(() => set(foo, 1))
-    .commit();
-  expect(deref(foo)).toBe(1);
-
-  tx1.retry().commit();
-
-  expect(deref(foo)).toBe(-1);
-});
-
-test("manual exec", () => {
-  let foo = ref(0);
-  let calls = 0;
-  let tx2 = transaction()
-    .add(() => {
-      calls++;
-      set(foo, 10);
-    })
-    .add(() => {
-      calls++;
-      alter(foo, (x) => x - 13);
+  test("throwing inside add does nothing (not executed yet)", () => {
+    tx.add(() => {
+      if (deref(foo) === 0) {
+        throw new Error("Invalid foo!");
+      }
+      set(foo, -1);
     });
 
-  for (let exec of tx2) {
-    exec();
-  }
+    expect(deref(foo)).toBe(0);
+  });
 
-  tx2.commit();
+  test("throwing inside commit doesn't mutate ref", () => {
+    let error;
+    try {
+      tx.commit();
+    } catch (e) {
+      error = e;
+    }
+    expect(error.message).toBe("Invalid foo!");
 
-  expect(deref(foo)).toBe(-3);
-  expect(calls).toBe(2);
+    expect(deref(foo)).toBe(0);
+  });
+
+  test("can't commit an aborted transaction", () => {
+    let error;
+    try {
+      tx.commit();
+    } catch (e) {
+      error = e;
+    }
+    expect(error.message).toBe(
+      "Cannot commit transaction which has been aborted. Retry it first"
+    );
+  });
+
+  test("retrying", () => {
+    transaction()
+      .add(() => set(foo, 1))
+      .commit();
+    expect(deref(foo)).toBe(1);
+
+    tx.retry().commit();
+
+    expect(deref(foo)).toBe(-1);
+  });
+});
+
+describe("iterable", () => {
+  test("for of", () => {
+    let foo = ref(0);
+    let calls = 0;
+    let tx = transaction()
+      .add(() => {
+        calls++;
+        set(foo, 10);
+      })
+      .add(() => {
+        calls++;
+        alter(foo, (x) => x - 13);
+      });
+
+    for (let exec of tx) {
+      exec();
+    }
+
+    tx.commit();
+
+    expect(deref(foo)).toBe(-3);
+    expect(calls).toBe(2);
+  });
+
+  test("destructuring", () => {
+    let foo = ref(0);
+
+    let tx = transaction();
+  });
 });
 
 // // // let nested = ref(0);
